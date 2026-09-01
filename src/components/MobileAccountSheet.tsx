9f218f503a9299b7852dbaa69c6cb245f9ccd4b7
Tho Tran <phucthotran@gmail.com>
2026-09-01 19:49:42 +0700

    fix: useless pin code change action on MobileAccountSheet

--- src/components/MobileAccountSheet.tsx
+++ src/components/MobileAccountSheet.tsx
@@ -1,12 +1,10 @@
 import { type User } from 'firebase/auth'
-import { KeyRound } from 'lucide-react'
 import { useTranslation } from 'react-i18next'
 
 import { AppShellUserCard } from './AppShellUserCard'
 import { LocaleToggle } from './LocaleToggle'
-import { usePinLock } from './PinLockProvider'
 import { ThemeToggle } from './ThemeToggle'
-import { Button, Drawer, DrawerContent, DrawerHeader, DrawerTitle } from './ui'
+import { Drawer, DrawerContent, DrawerHeader, DrawerTitle } from './ui'
 
 type Props = {
   open: boolean
@@ -16,7 +14,6 @@ type Props = {
 
 export function MobileAccountSheet({ onOpenChange, open, user }: Props) {
   const { t } = useTranslation()
-  const { hasPin, openChangePin, openSetPin } = usePinLock()
 
   return (
     <Drawer direction="bottom" open={open} onOpenChange={onOpenChange} shouldScaleBackground={false}>
@@ -27,21 +24,6 @@ export function MobileAccountSheet({ onOpenChange, open, user }: Props) {
         </DrawerHeader>
         <div className="flex flex-col gap-3 px-4 pb-[max(env(safe-area-inset-bottom),1.5rem)]">
           {user ? <AppShellUserCard user={user} /> : null}
-          {user ? (
-            <Button
-              type="button"
-              variant="outline"
-              className="w-full"
-              onClick={() => {
-                onOpenChange(false)
-                if (hasPin) openChangePin()
-                else openSetPin()
-              }}
-            >
-              <KeyRound className="h-4 w-4" />
-              {hasPin ? t('nav.changePin') : t('nav.setPin')}
-            </Button>
-          ) : null}
           <div className="flex items-center justify-center gap-2">
             <LocaleToggle />
             <ThemeToggle />
